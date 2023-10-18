25eb6441eae342ef490e7f504fb8010ac5678e9f
liuchenxi 00024539 <liuchenxi@hihonor.com>
2023-10-18 15:30:46 +0800

    修改配置

--- src/modules/user/config.ts
+++ src/modules/user/config.ts
@@ -41,7 +41,7 @@ export default () => {
       // 刷新token 过期时间，单位秒
       refreshExpire: 60 * 60 * 24 * 30,
       // jwt 秘钥
-      secret: 'cool-app-xxxxxx',
+      secret: '8c6f4d106d8011ee83a6cfb10e6b24cc',
     },
   } as ModuleConfig;
 };
